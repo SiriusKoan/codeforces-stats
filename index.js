78f449c27c8b01279b3c30ca5a0b06a7a81fe40e
SiriusKoan <k.peihsun@gmail.com>
2021-06-13 18:20:51 +0800

    Add card structure

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 const cf_api_url = "https://codeforces.com/api/user.info?handles="
+var parameters = new URLSearchParams(window.location.search);
+const handle = parameters.get("handle");
+const theme = parameters.get("theme");
+const use_contributions = parameters.get("contributions") == "true" ? true : false;
+const use_friends = parameters.get("friends") == "true" ? true : false;
 
-window.onload = makeCard(window.location.search)
+window.onload = makeCard(handle, theme, use_contributions, use_friends)
 
-
-function makeCard(query) {
-	var parameters = new URLSearchParams(query);
-	var handle = parameters.get("handle");
-	var theme = parameters.get("theme");
-	var data = fetchData(handle);
-	console.log(data)
-}
-
-async function fetchData(handle) {
+async function makeCard(handle, theme, use_contributions, use_friends) {
 	var url = cf_api_url + handle;
 	const response = await fetch(url);
 	const json = await response.json();
-	console.log(json)
-	return json
+	console.log(json);
+	if (json["status"] == "OK") {
+		make(json["result"][0], theme, use_contributions, use_friends);
+	}
 }
 
 
-function convert(json) {
-	if (json["status"] == "OK") {
-		var result = json["result"][0];
+function make(json, theme, use_contributions, use_friends) {
+	var rank = document.getElementById("rank");
+	rank.textContent = json["rank"];
+	var handle = document.getElementById("handle");
+	handle.textContent = json["handle"];
+	var org = document.getElementById("org");
+	org.textContent = json["organization"];
+	var rating = document.getElementById("rating");
+	rating.textContent = json["rating"];
+	var max_rating = document.getElementById("max_rating");
+	max_rating.textContent = json["maxRating"];
+	if (use_contributions) {
+		var contributions = document.getElementById("contributions");
+		contributions.textContent = json["contributions"];
+	}
+	if (use_friends) {
+		var friends = document.getElementById("friends");
+		friends.textContent = json["friends"];
 	}
 }
