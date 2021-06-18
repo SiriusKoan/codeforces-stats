3ea6870f0703509c7ff06e84be878075a599e6fe
SiriusKoan <k.peihsun@gmail.com>
2021-06-18 22:13:49 +0800

    Use svg and add 404 response

--- index.js
+++ index.js
@@ -16,31 +16,34 @@ const svg_template = `
 </svg>`;
 
 addEventListener("fetch", (event) => {
-	event.respondWith(
-		makeCard(event.request).catch(
-			(err) => new Response(err.stack, { status: 500 })
-		)
-	);
+    event.respondWith(
+        makeCard(event.request).catch(
+            (err) => new Response(err.stack, { status: 500 })
+        )
+    );
 });
 
 async function makeCard(request) {
-	var parameters = new URLSearchParams(new URL(request.url).searchParams);
-	let handle = parameters.get("handle");
-	let theme = parameters.get("theme");
-	let use_contributions = parameters.get("contributions") == "true" ? true : false;
-	let use_friends = parameters.get("friends") == "true" ? true : false;
-	let url = cf_api_url + handle;
-	const response = await fetch(url);
-	const json = await response.json();
-	console.log(json);
-	if (json["status"] == "OK") {
-		return make(json["result"][0], theme, use_contributions, use_friends);
-	}
+    var parameters = new URLSearchParams(new URL(request.url).searchParams);
+    let handle = parameters.get("handle");
+    let theme = parameters.get("theme");
+    let use_contributions = parameters.get("contributions") == "true" ? true : false;
+    let use_friends = parameters.get("friends") == "true" ? true : false;
+    let url = cf_api_url + handle;
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(json);
+    if (json["status"] == "OK") {
+        return make(json["result"][0], theme, use_contributions, use_friends);
+    }
+    else {
+        return new Response("Not found", { status: 404 })
+    }
 }
 
 
 function make(json, theme, use_contributions, use_friends) {
-	svg = `
+    svg = `
   <svg xmlns="http://www.w3.org/2000/svg" class="card" width="500" height="200" viewBox="0 0 500 200" fill="none">
       <script src="index.js"></script>
       <g>
@@ -51,14 +54,14 @@ function make(json, theme, use_contributions, use_friends) {
       <g>
           <text id="rating" fill="black">${json["rating"]}</text>
           <text id="max_rating" fill="black">${json["maxRating"]}</text>
-          <text id="contributions" fill="black">${json["contributions"]}</text>
-          <text id="friends" fill="black">${json["friends"]}</text>
+          <text id="contributions" fill="black">${use_contributions ? json["contributions"] : ""}</text>
+          <text id="friends" fill="black">${use_friends ? json["friends"] : ""}</text>
       </g>
   </svg>`;
-	return new Response(svg, {
-		headers: {
-			"Content-Type": "text/html; charset=utf-8",
-		},
-	});
+    return new Response(svg, {
+        headers: {
+            "Content-Type": "image/svg+xml; charset=utf-8",
+        },
+    });
 }
 
